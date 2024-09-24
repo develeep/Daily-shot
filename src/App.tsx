78270a39e1fe577fca5024599b89ffdeccc1bc89
Lee <dltmdgns0508@gmail.com>
2024-09-24 23:20:02 +0900

    기본 폴더 구조 설정 및 임시 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/layout.tsx';
+import Home from './routes/home.tsx';
+import Profile from './routes/profile.tsx';
+import Login from './routes/login.tsx';
+import CreateAccount from './routes/create-account.tsx';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/globalStyles.ts';
+import { Theme } from './styles/globalTheme.ts';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount />,
+  },
+]);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
